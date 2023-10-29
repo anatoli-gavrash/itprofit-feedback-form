347c7e815bf2bdf22d05cff5a8a89e5a6cdb785b
Anatoli Gavrash <gaver5@yandex.ru>
2023-10-29 18:30:58 +0300

    Add functionality in the index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import IMask from 'imask';
+import {
+  endMessageButtonHandler,
+  formHandler,
+  resetButtonHandler
+} from './utils/handlers';
 import './index.scss';
+
+(function () {
+  const feedbackForm = document.querySelector('.feedback__form');
+  const phoneField = feedbackForm.querySelector('#id-phone');
+  const formResetButton = feedbackForm.querySelector('.feedback__form-buttons-button.alt-button');
+  const endMessageButton = feedbackForm.querySelector('.feedback__form-success-button');
+
+  IMask(phoneField, {mask: '+{375} (00) 000-00-00'});
+
+  feedbackForm.addEventListener('submit', formHandler);
+  formResetButton.addEventListener('click', resetButtonHandler);
+  endMessageButton.addEventListener('click', endMessageButtonHandler);
+}());
